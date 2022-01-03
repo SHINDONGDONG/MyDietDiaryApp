64558322c8efcfbd78af534a26461ff5131b1f8d
mincheol <homerjay@naver.com>
2022-01-04 07:27:24 +0900

    Realm Data, WeightTextField, keyboard, done item

--- MyDietDiaryApp/EditorViewController.swift
+++ MyDietDiaryApp/EditorViewController.swift
@@ -9,8 +9,28 @@ import Foundation
 import UIKit
 
 class EditorViewController: UIViewController {
+    //체중의 TextField를 변수로 선언한다.
+    @IBOutlet weak var inputWeightTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        configureWeightTextField()
+    }
+    
+    @objc func didTapDone(){
+        view.endEditing(true)
+    }
+    
+    func configureWeightTextField(){
+        //toolbarRect의 사이즈를 정의한다.
+        let toolBarRect = CGRect(x: 0, y: 0, width: view.frame.size.width, height: 35)
+        //정의한 사이즈를 toolbar에 추가해준다.
+        let toolBar = UIToolbar(frame: toolBarRect)
+        //doneitem을 버튼, 타겟, 액션을 추가해준다
+        let doneItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(didTapDone))
+        //toolbar에 doneItem을 []로 셋팅해준다(복수의 아이템들이 들어올 수 있기에)
+        toolBar.setItems([doneItem], animated: true)
+        //inputweightTextField에 악세서리로 추가해준다.
+        inputWeightTextField.inputAccessoryView = toolBar
     }
 }
---
+++ MyDietDiaryApp/WeightRecored.swift
@@ -0,0 +1,20 @@
+//
+//  WeightRecored.swift
+//  MyDietDiaryApp
+//
+//  Created by 申民鐡 on 2022/01/04.
+//
+
+import Foundation
+import RealmSwift
+
+class WeightRecored: Object {
+    override class func primaryKey() -> String? {
+        return "id"
+    }
+    
+    @objc dynamic var id: String = UUID().uuidString
+    @objc dynamic var date: Date = Date()
+    @objc dynamic var weight: Double = 0.0
+    
+}
